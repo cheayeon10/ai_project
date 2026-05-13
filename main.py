327ca2b121049c7e0fa38a2e151cebd53db502e9
cheayeon10 <a01074082857@gmail.com>
2026-05-13 13:51:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ b=st.selectbox('좋아하는 음식을 선택하세요!',['튀김','햄버거','
 if st.button('인사말 생성'):
   st.write(a+'님!, 안녕하세요!')
   st.info('반갑습니다')
-  st.warnung(b+'음식을 좋아하시는군요')
+  st.warning(b+'음식을 좋아하시는군요')
   st.error('잘 부탁해요!')
